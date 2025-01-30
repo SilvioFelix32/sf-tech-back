dfdd61986f99e9da5b60447306e2dde5e1931768
Silvio <silvio.felix@sysmap.com.br>
2025-01-30 15:35:27 -0300

    fix: redis certficate

--- src/domain/services/redis/redis.service.ts
+++ src/domain/services/redis/redis.service.ts
@@ -1,19 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import Redis from 'ioredis';
 import { env } from '../../../shared/config/env';
+import fs from 'fs';
+import path from 'path';
+
+const certPath = path.resolve(process.cwd(), 'redis-ca.crt');
+const caCert = fs.readFileSync(certPath, 'utf8');
 
 @Injectable()
 export class RedisService extends Redis {
   constructor() {
-    super({
+    if (!fs.existsSync(certPath)) {
+      console.error('Certified path not found:', certPath);
+      process.exit(1);
+    }
+
+    if (!caCert) {
+      console.error('Certified not found');
+      process.exit(1);
+    }
+
+    const redisClient = {
       host: env.REDIS_HOST,
       username: env.REDIS_USER,
       password: env.REDIS_PASSWORD,
       port: env.REDIS_PORT,
       tls: {
+        ca: caCert,
         rejectUnauthorized: false,
       },
-    });
+      showFriendlyErrorStack: true,
+    };
+
+    console.info('RedisService.constructor(), redisClient:', redisClient);
+    super(redisClient);
 
     super.on('error', (err) => {
       console.info('Error on Redis Service');
